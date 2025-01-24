728d4e8aa2305fcc69a18702b0453aad42e5ebd0
Marc Roethlisberger <m.roethlisberger477@gmail.com>
2025-01-24 16:56:31 +0100

    Added minimal hyprland setup

---
+++ hypr/.config/hypr/scripts/lidclosed.sh
@@ -0,0 +1,10 @@
+hyprctl keyword monitor "eDP-1, disable"
+
+#
+## --> If ever needed, check this and adapt.
+#
+#
+#if [ "$(acpi -a)" == "Adapter 0: on-line" ]
+#then
+#hyprctl keyword monitor "eDP-1, disable"
+#fi
